dd638f02804f31f1e06d2bec215172970915c351
Arsh2 <pathanarshkhan@gmail.com>
2023-05-27 10:29:44 +0530

    Added Card Component

--- dineout/pages/test.js
+++ dineout/pages/test.js
@@ -1,8 +1,11 @@
+import MediaCard, { DummyCard } from "@/components/Card";
 import Payment from "@/components/Payment";
+
 import React from "react";
 
 function test() {
-  return <Payment></Payment>;
+  // return <Payment></Payment>;
+  return <DummyCard></DummyCard>;
 }
 
 export default test;
