6a439cbd6c012e65eeeb5a441d192fc449f1aaff
Arsh2 <pathanarshkhan@gmail.com>
2023-05-30 16:40:02 +0530

    Added Drawer

--- dineout/pages/restaurant/index.js
+++ dineout/pages/restaurant/index.js
@@ -1,7 +1,13 @@
 import { withRole } from "@/components/Middleware";
+import ResponsiveDrawer from "@/components/Dashboard/Drawer";
 
 const Restaurant = () => {
-  return <div>Manage the restaurant</div>;
+  return (
+    <div>
+      Manage the restaurant
+      <ResponsiveDrawer></ResponsiveDrawer>
+    </div>
+  );
 };
 
 export default Restaurant;
