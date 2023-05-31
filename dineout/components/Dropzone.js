463520ca8ce1517c9a6adaa4f4d9de1f48b55ba9
Arsh2 <pathanarshkhan@gmail.com>
2023-05-31 16:51:22 +0530

    Added Cloudinary in React and Changed Image Fie

--- dineout/components/Dropzone.js
+++ dineout/components/Dropzone.js
@@ -1,61 +1,65 @@
 // packages
-import { useEffect, useState } from 'react';
-import ReactDropzone from 'react-dropzone';
-import { styled, IconButton } from '@mui/material';
-import { Delete } from '@mui/icons-material';
+import { useEffect, useState } from "react";
+import ReactDropzone from "react-dropzone";
+import { styled, IconButton } from "@mui/material";
+import { Delete } from "@mui/icons-material";
 
-const StyledDropzone = styled('div')({
-    padding: '20px',
-    border: '2px dashed #aaa',
-    borderRadius: '5px',
-    textAlign: 'center',
-    cursor: 'pointer',
-    '&:hover': {
-        borderColor: '#777',
-    },
+const StyledDropzone = styled("div")({
+  padding: "20px",
+  border: "2px dashed #aaa",
+  borderRadius: "5px",
+  textAlign: "center",
+  cursor: "pointer",
+  "&:hover": {
+    borderColor: "#777",
+  },
 });
 
-const FileRow = styled('div')({
-    display: 'flex',
-    alignItems: 'center',
-    marginTop: '10px',
-    paddingInline: '15px',
+const FileRow = styled("div")({
+  display: "flex",
+  alignItems: "center",
+  marginTop: "10px",
+  paddingInline: "15px",
 });
 
-const FileName = styled('span')({
-    flex: 1,
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    whiteSpace: 'nowrap',
+const FileName = styled("span")({
+  flex: 1,
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+  whiteSpace: "nowrap",
 });
 
 const Dropzone = ({ allFiles, handleDrop, handleDelete, title }) => {
-    const [files, setFiles] = useState([]);
+  const [files, setFiles] = useState([]);
 
-    useEffect(() => {
-        setFiles(allFiles);
-    }, [allFiles]);
+  useEffect(() => {
+    setFiles(allFiles);
+  }, [allFiles]);
 
-    return (
-        <ReactDropzone onDrop={handleDrop} multiple>
-            {({ getRootProps, getInputProps }) => (
-                <section>
-                    <StyledDropzone {...getRootProps()}>
-                        <input {...getInputProps()} />
-                        <p>Drag 'n' drop {title} files here, or click to select files</p>
-                    </StyledDropzone>
-                    {files && files?.map((file, index) => (
-                        <FileRow key={index}>
-                            <FileName>{file.file.name}</FileName>
-                            <IconButton color="secondary" onClick={() => handleDelete(index)}>
-                                <Delete />
-                            </IconButton>
-                        </FileRow>
-                    ))}
-                </section>
-            )}
-        </ReactDropzone>
-    );
+  return (
+    <ReactDropzone onDrop={handleDrop} multiple>
+      {({ getRootProps, getInputProps }) => (
+        <section>
+          <StyledDropzone {...getRootProps()}>
+            <input {...getInputProps()} />
+            <p>Drag 'n' drop {title} files here, or click to select files</p>
+          </StyledDropzone>
+          {files &&
+            files?.map((file, index) => (
+              <FileRow key={index}>
+                <FileName>{file.file.name}</FileName>
+                <IconButton
+                  color="secondary"
+                  onClick={() => handleDelete(index)}
+                >
+                  <Delete />
+                </IconButton>
+              </FileRow>
+            ))}
+        </section>
+      )}
+    </ReactDropzone>
+  );
 };
 
 export default Dropzone;
--- dineout/pages/restaurant/add.js
+++ dineout/pages/restaurant/add.js
@@ -6,100 +6,101 @@ import {
   TextField,
   Grid,
   Button,
-} from '@mui/material';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
+} from "@mui/material";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 // layouts
-import RestaurantLayout from '@/layouts/restaurant';
+import RestaurantLayout from "@/layouts/restaurant";
 // components
-import Select from '@/components/Select';
-import Dropzone from '@/components/Dropzone';
+import Select from "@/components/Select";
+import Dropzone from "@/components/Dropzone";
 // store
-import { useCreateRestaurantMutation } from '@/store/api/restaurant';
-
+import { useCreateRestaurantMutation } from "@/store/api/restaurant";
+import axios from "axios";
+import { useState } from "react";
 
 // _mock api data
 const apiTags = [
   {
-    id: '1',
-    name: 'Tag 1',
-    image: 'http://localhost:8000/api/images/image1.png',
+    id: "1",
+    name: "Tag 1",
+    image: "http://localhost:8000/api/images/image1.png",
   },
   {
-    id: '2',
-    name: 'Tag 2',
-    image: 'http://localhost:8000/api/images/image2.png',
+    id: "2",
+    name: "Tag 2",
+    image: "http://localhost:8000/api/images/image2.png",
   },
   {
-    id: '3',
-    name: 'Tag 3',
-    image: 'http://localhost:8000/api/images/image3.png',
+    id: "3",
+    name: "Tag 3",
+    image: "http://localhost:8000/api/images/image3.png",
   },
   {
-    id: '4',
-    name: 'Tag 4',
-    image: 'http://localhost:8000/api/images/image4.png',
+    id: "4",
+    name: "Tag 4",
+    image: "http://localhost:8000/api/images/image4.png",
   },
   {
-    id: '5',
-    name: 'Tag 5',
-    image: 'http://localhost:8000/api/images/image5.png',
+    id: "5",
+    name: "Tag 5",
+    image: "http://localhost:8000/api/images/image5.png",
   },
 ];
 
 const apiCuisines = [
   {
-    id: '1',
-    name: 'Cuisines 1',
+    id: "1",
+    name: "Cuisines 1",
   },
   {
-    id: '2',
-    name: 'Cuisines 2',
+    id: "2",
+    name: "Cuisines 2",
   },
   {
-    id: '3',
-    name: 'Cuisines 3',
+    id: "3",
+    name: "Cuisines 3",
   },
   {
-    id: '4',
-    name: 'Cuisines 4',
+    id: "4",
+    name: "Cuisines 4",
   },
   {
-    id: '5',
-    name: 'Cuisines 5',
+    id: "5",
+    name: "Cuisines 5",
   },
 ];
 
 const apiTypes = [
   {
-    id: '1',
-    name: 'Veg',
+    id: "1",
+    name: "Veg",
   },
   {
-    id: '2',
-    name: 'Egg',
+    id: "2",
+    name: "Egg",
   },
   {
-    id: '3',
-    name: 'Non Veg',
+    id: "3",
+    name: "Non Veg",
   },
 ];
 
 const initialValues = {
-  name: '',
-  locality: '',
-  address: '',
-  city: '',
-  avgCost: '',
-  coordinates: '',
-  phoneNumber: '',
-  description: '',
+  name: "",
+  locality: "",
+  address: "",
+  city: "",
+  avgCost: "",
+  coordinates: "",
+  phoneNumber: "",
+  description: "",
   tags: [],
   cuisines: [],
   types: [],
-  openingTime: '',
-  closingTime: '',
-  unitCharge: '',
+  openingTime: "",
+  closingTime: "",
+  unitCharge: "",
   restaurantImages: [],
   menuImages: [],
 };
@@ -108,44 +109,61 @@ const validationSchema = Yup.object({
   name: Yup.string()
     .min(2)
     .max(25)
-    .required('Please enter your restaurant name'),
-  locality: Yup.string().min(2).max(50).required('Please enter your locality'),
-  address: Yup.string().min(2).max(70).required('Please enter your address'),
-  city: Yup.string().min(2).max(15).required('Please enter your city'),
-  avgCost: Yup.number().required('Enter average cost').positive().integer(),
-  coordinates: Yup.string().min(2).required('Enter Your location link'),
+    .required("Please enter your restaurant name"),
+  locality: Yup.string().min(2).max(50).required("Please enter your locality"),
+  address: Yup.string().min(2).max(70).required("Please enter your address"),
+  city: Yup.string().min(2).max(15).required("Please enter your city"),
+  avgCost: Yup.number().required("Enter average cost").positive().integer(),
+  coordinates: Yup.string().min(2).required("Enter Your location link"),
   phoneNumber: Yup.string()
-    .matches(/^\d{10,}$/, 'Invalid phone number')
-    .required('Enter your phone number'),
+    .matches(/^\d{10,}$/, "Invalid phone number")
+    .required("Enter your phone number"),
   description: Yup.string()
     .min(2)
     .max(100)
-    .required('Please enter description'),
-  unitCharge: Yup.number().required('Enter charges').positive().integer(),
+    .required("Please enter description"),
+  unitCharge: Yup.number().required("Enter charges").positive().integer(),
 });
 
 const Add = () => {
+  const [imageUrls, setImageUrls] = useState([]);
+
+  const addData = (newData) => {
+    setImageUrls([...imageUrls, newData]);
+  };
+
   const [createRestaurant] = useCreateRestaurantMutation();
 
   const onSubmit = async (values) => {
     const formData = new FormData();
-    formData.append('name', values.name);
-    formData.append('locality', values.locality);
-    formData.append('city', values.city);
-    formData.append('address', values.address);
-    formData.append('description', values.description);
-    formData.append('coordinates', values.coordinates);
-    formData.append('phone_number', values.phoneNumber);
-    formData.append('avg_cost', values.avgCost);
-    formData.append('opening_time', values.openingTime);
-    formData.append('closing_time', values.closingTime);
-    formData.append('tag_list', values.tags.map(tag => tag.id));
-    formData.append('types_list', values.types.map(type => type.id));
-    formData.append('cuisines_list', values.cuisines.map(cuisine => cuisine.id));
-    formData.append('unit_charge', values.unitCharge);
-    formData.append('manager', 27);
-    formData.append('uploaded_images', values.restaurantImages);
-    formData.append('uploaded_menuImages', values.menuImages);
+    formData.append("name", values.name);
+    formData.append("locality", values.locality);
+    formData.append("city", values.city);
+    formData.append("address", values.address);
+    formData.append("description", values.description);
+    formData.append("coordinates", values.coordinates);
+    formData.append("phone_number", values.phoneNumber);
+    formData.append("avg_cost", values.avgCost);
+    formData.append("opening_time", values.openingTime);
+    formData.append("closing_time", values.closingTime);
+    // formData.append(
+    //   "tag_list",
+    //   values.tags.map((tag) => tag.id)
+    // );
+    // formData.append(
+    //   "types_list",
+    //   values.types.map((type) => type.id)
+    // );
+    // formData.append(
+    //   "cuisines_list",
+    //   values.cuisines.map((cuisine) => cuisine.id)
+    // );
+    formData.append("unit_charge", values.unitCharge);
+    formData.append("manager", 2);
+    formData.append("uploaded_images", imageUrls);
+
+    // formData.append("uploaded_images", values.restaurantImages);
+    // formData.append("uploaded_menuImages", values.menuImages);
 
     // const formatedValues = {
     //   // ...values,
@@ -169,7 +187,7 @@ const Add = () => {
     // };
 
     createRestaurant(formData);
-  }
+  };
 
   return (
     <RestaurantLayout>
@@ -277,7 +295,7 @@ const Add = () => {
                         <Select
                           options={apiTags}
                           value={values.tags}
-                          onChange={(values) => setFieldValue('tags', values)}
+                          onChange={(values) => setFieldValue("tags", values)}
                           placeholder="Tags"
                         />
                       </Grid>
@@ -285,7 +303,9 @@ const Add = () => {
                         <Select
                           options={apiCuisines}
                           value={values.cuisines}
-                          onChange={(values) => setFieldValue('cuisines', values)}
+                          onChange={(values) =>
+                            setFieldValue("cuisines", values)
+                          }
                           placeholder="Cuisines"
                         />
                       </Grid>
@@ -293,7 +313,7 @@ const Add = () => {
                         <Select
                           options={apiTypes}
                           value={values.types}
-                          onChange={(values) => setFieldValue('types', values)}
+                          onChange={(values) => setFieldValue("types", values)}
                           placeholder="Types"
                         />
                       </Grid>
@@ -332,42 +352,74 @@ const Add = () => {
                       <Grid container item xs={12} spacing={3}>
                         <Grid item xs={12} sm={6}>
                           <Dropzone
-                          title="restaurant"
+                            title="restaurant"
                             allFiles={values.restaurantImages}
-                            handleDrop={(acceptedFiles) => {
-                              const newFiles = acceptedFiles.map((file) => ({
-                                file,
-                                preview: URL.createObjectURL(file),
-                              }));
+                            handleDrop={(files) => {
+                              const uploaders = files.map((file) => {
+                                const formData = new FormData();
+                                formData.append("file", file);
+                                formData.append(
+                                  "tags",
+                                  `codeinfuse, medium, gist`
+                                );
+                                formData.append("upload_preset", "dineout");
+                                formData.append("api_key", "257987867351426");
+                                formData.append(
+                                  "timestamp",
+                                  (Date.now() / 1000) | 0
+                                );
 
-                              setFieldValue('restaurantImages', [...values.restaurantImages, ...newFiles]);
+                                // Make an AJAX upload request using Axios (replace Cloudinary URL below with your own)
+                                return axios
+                                  .post(
+                                    "https://api.cloudinary.com/v1_1/dhe9hmzbn/image/upload",
+                                    formData,
+                                    {
+                                      headers: {
+                                        "X-Requested-With": "XMLHttpRequest",
+                                      },
+                                    }
+                                  )
+                                  .then((response) => {
+                                    const data = response.data;
+                                    const fileURL = data.secure_url; // You should store this URL for future references in your app
+                                    console.log(data);
+                                    addData(fileURL);
+                                  });
+                              });
+
+                              // Once all the files are uploaded
+                              axios.all(uploaders).then(() => {
+                                // ... perform after upload is successful operation
+                              });
                             }}
-
                             handleDelete={(index) => {
                               const newFiles = [...values.restaurantImages];
                               newFiles.splice(index, 1);
-                              setFieldValue('restaurantImages', newFiles);
+                              setFieldValue("restaurantImages", newFiles);
                             }}
                           />
                         </Grid>
                         <Grid item xs={12} sm={6} md={6} lg={6}>
-                          <Dropzone
-                          title="menu"
+                          {/* <Dropzone
+                            title="menu"
                             allFiles={values.menuImages}
                             handleDrop={(acceptedFiles) => {
                               const newFiles = acceptedFiles.map((file) => ({
                                 file,
                                 preview: URL.createObjectURL(file),
                               }));
-                              setFieldValue('menuImages', [...values.menuImages, ...newFiles]);
+                              setFieldValue("menuImages", [
+                                ...values.menuImages,
+                                ...newFiles,
+                              ]);
                             }}
-
                             handleDelete={(index) => {
                               const newFiles = [...values.menuImages];
                               newFiles.splice(index, 1);
-                              setFieldValue('menuImages', newFiles);
+                              setFieldValue("menuImages", newFiles);
                             }}
-                          />
+                          /> */}
                         </Grid>
                       </Grid>
                       <Grid item xs={12}>
