cf2dc86c6c7cea7fed6c85261a38ce78fcf69c41
Arshkhan <pathanarshkhan@gmail.com>
2023-06-02 12:06:52 +0530

    Summary Somehow Completed

--- dineout/pages/restaurant/index.js
+++ dineout/pages/restaurant/index.js
@@ -5,27 +5,52 @@ import { styled, useTheme } from "@mui/material/styles";
 import { Grid } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import { selectCurrentUser } from "@/store/slices/auth";
-import { useSelector, useEffect } from "react-redux";
+import { useSelector } from "react-redux";
 import { useGetRestaurantEarningsQuery } from "@/store/api/restaurant";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 import PrimarySearchAppBar from "@/components/Dashboard/Appbar";
 import Summary from "@/sections/restaurant/home/Summary";
+
 const DrawerHeader = styled("div")(({ theme }) => ({
   display: "flex",
   alignItems: "center",
   justifyContent: "flex-end",
   padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
+
   ...theme.mixins.toolbar,
 }));
-const Restaurant = () => {
+const Restaurant = ({}) => {
   const user = useSelector(selectCurrentUser);
-  const {data}= useGetRestaurantEarningsQuery(39, {
-    pollingInterval: 20000,
-  });
+  const [data, setData] = useState();
+  const [stats, setStats] = useState();
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const requests = [
+          axios.get(
+            `http://127.0.0.1:8000/api/restaurant/restaurants/${user.id}/earnings/`
+          ),
+          axios.get(
+            `http://127.0.0.1:8000/api/restaurant/restaurants/${user.id}/bookings/stats`
+          ),
+        ];
 
-  console.log(user);
+        const responses = await Promise.all(requests);
+
+        setData(responses[0].data);
+        setStats(responses[1].data);
+      } catch (error) {
+        console.error("API Error:", error);
+      }
+    };
+
+    fetchData();
+  }, [user?.id]);
+
+  console.log(stats);
 
   return (
     <div>
@@ -41,7 +66,7 @@ const Restaurant = () => {
           </Box>
 
           <Box>
-            <Summary data={data} />
+            <Summary data={data} stats={stats} graph={data?.graph} />
           </Box>
         </Box>
       </Box>
--- dineout/sections/restaurant/home/Summary.js
+++ dineout/sections/restaurant/home/Summary.js
@@ -10,52 +10,9 @@ import {
   Legend,
 } from "recharts";
 
-const data1 = [
-  {
-    name: "dat",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "dat",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "dat",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "dat",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "dat",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "dat",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "dat",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+const Summary = ({ data, stats, graph }) => {
+  console.log(graph);
 
-const Summary = ({ data }) => {
   return (
     <>
       {" "}
@@ -68,16 +25,19 @@ const Summary = ({ data }) => {
             <Widget title="Week Earning" amount={data?.last_week} />
           </Grid>
           <Grid item xs={12} md={6} lg={3} xl={3}>
-            <Widget title="Todays Booking" amount="2" />
+            <Widget title="Todays Booking" amount={stats?.today_bookings} />
           </Grid>
           <Grid item xs={12} md={6} lg={3} xl={3}>
-            <Widget title="Upcoming Bookings" amount="20" />
+            <Widget
+              title="Upcoming Bookings"
+              amount={stats?.upcoming_bookings}
+            />
           </Grid>{" "}
           <Grid item xs={6}>
             <LineChart
               width={500}
               height={300}
-              data={data1}
+              data={graph}
               margin={{
                 top: 5,
                 right: 30,
@@ -86,24 +46,23 @@ const Summary = ({ data }) => {
               }}
             >
               <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
+              <XAxis dataKey="date" />
               <YAxis />
               <Tooltip />
               <Legend />
               <Line
                 type="monotone"
-                dataKey="pv"
+                dataKey="total_earnings"
                 stroke="#8884d8"
                 activeDot={{ r: 8 }}
               />
-              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
             </LineChart>
           </Grid>
           <Grid item xs={6}>
             <LineChart
               width={500}
               height={300}
-              data={data1}
+              data={graph}
               margin={{
                 top: 5,
                 right: 30,
@@ -112,17 +71,16 @@ const Summary = ({ data }) => {
               }}
             >
               <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
+              <XAxis dataKey="date" />
               <YAxis />
               <Tooltip />
               <Legend />
               <Line
                 type="monotone"
-                dataKey="pv"
+                dataKey="total_bookings"
                 stroke="#8884d8"
                 activeDot={{ r: 8 }}
               />
-              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
             </LineChart>
           </Grid>
         </Grid>
