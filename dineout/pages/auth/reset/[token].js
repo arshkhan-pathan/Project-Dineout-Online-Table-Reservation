964f61695f3a3e9c23f3e9654d82464cdca3887a
Arshkhan <pathanarshkhan@gmail.com>
2023-06-13 17:14:13 +0530

    Confirm Password Token

--- dineout/pages/auth/reset/[token].js
+++ dineout/pages/auth/reset/[token].js
@@ -1,13 +1,18 @@
 // packages
 import { useRouter } from "next/router";
+import ForgotPassword from "@/components/ForgotPassword";
 // layouts
-
+import UserLayout from "@/layouts/user";
 const ResetPassword = () => {
   const router = useRouter();
   const token = router.query.token;
   let tokens = token?.split("&");
 
-  return <>Forgot Page</>;
+  return (
+    <UserLayout>
+      <ForgotPassword></ForgotPassword>
+    </UserLayout>
+  );
 };
 
 export default ResetPassword;
