8a8e7ae59ef949495a0d5d842c7aa8d42a06e166
Arshkhan <pathanarshkhan@gmail.com>
2023-06-22 19:10:30 +0530

    Menu Images View

--- dineout/sections/user/restaurant/FoodMenu.js
+++ dineout/sections/user/restaurant/FoodMenu.js
@@ -1,13 +1,12 @@
 // import styled from "styled-components";
-import Modal from "@mui/material/Modal";
-import { useState } from "react";
 import Image from "next/image";
 import { Box, Grid, Stack, Typography, styled } from "@mui/material";
+import { PhotoProvider, PhotoView } from "react-photo-view";
 
 // styles
 const StyedWrapper = styled(Box)({
-  padding: '16px 24px 24px',
-  borderRadius: '4px'
+  padding: "16px 24px 24px",
+  borderRadius: "4px",
 });
 
 const style = {
@@ -23,14 +22,16 @@ const style = {
 };
 
 const FoodMenu = ({ menu }) => {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
   return (
     <>
       <StyedWrapper>
         <Stack direction="column" spacing={2}>
-          <Typography variant="h6" gutterBottom id="AboutUs" sx={{fontSize: '18px', fontWeight: 'bold'}}>
+          <Typography
+            variant="h6"
+            gutterBottom
+            id="AboutUs"
+            sx={{ fontSize: "18px", fontWeight: "bold" }}
+          >
             Menu
           </Typography>
           <Box>
@@ -39,38 +40,17 @@ const FoodMenu = ({ menu }) => {
                 menu.map((image) => {
                   return (
                     <Grid item xs={2} key={image.image}>
-                      <Image
-                        src={image.image}
-                        height={100}
-                        width={100}
-                        alt="Image"
-                        onClick={handleOpen}
-                        style={{ objectFit: 'cover' }}
-                      />
-
-                      <Modal
-                        open={open}
-                        onClose={handleClose}
-                        aria-labelledby="modal-modal-title"
-                        aria-describedby="modal-modal-description"
-                        sx={{
-                          "& img": {
-                            width: '100%',
-                            height: '80vh'
-                          }
-                        }}
-                      >
-                        <Box sx={style}>
+                      <PhotoProvider>
+                        <PhotoView key={image.image} src={image.image}>
                           <Image
                             src={image.image}
-                            height={550}
-                            width={500}
+                            height={100}
+                            width={100}
                             alt="Image"
-                            onClick={handleOpen}
-                            
+                            style={{ objectFit: "cover" }}
                           />
-                        </Box>
-                      </Modal>
+                        </PhotoView>
+                      </PhotoProvider>
                     </Grid>
                   );
                 })}
