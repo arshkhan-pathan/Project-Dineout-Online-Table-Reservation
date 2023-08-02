ff5c34a5eb334d3edef1a658636006ed83dde864
Arshkhan Pathan <80395000+arshkhan-pathan@users.noreply.github.com>
2023-08-02 17:38:17 +0530

    Update _document.js

--- dineout/pages/_document.js
+++ dineout/pages/_document.js
@@ -3,8 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+    <head>
+    <meta name="google-site-verification" content="enODw4izomS6lCG0oVgUmYe63JbPJobs3n6SIlbU5VU" />
+    </head>
       <Head />
-      <body>
+    
+    <body>
         <div id="overlays"></div>
         <Main />
         <NextScript />
