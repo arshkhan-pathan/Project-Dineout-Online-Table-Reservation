d9cf6978f0880282796445f8fb8e9a8b945a4ede
Arshkhan <pathanarshkhan@gmail.com>
2025-10-12 23:34:03 +0530

    updated files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-rv#1e(-xh3t+%=tg5kr@53usf+-jek2v5ju7wwg!pju3$o@6f$'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = True
 
